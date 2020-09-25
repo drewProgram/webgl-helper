062a0b892727c0114f12b16510b6cc2d9a302ae4
Andrew Filgueiras <andrew.lascasas@gmail.com>
2020-09-25 16:45:03 -0300

    feat: added canvasResize, createProgram, createShader and first version of createProgramFromSources

--- base.js
+++ base.js
@@ -1,36 +1,3 @@
-/*
- * Copyright 2012, Gregg Tavares.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Gregg Tavares. nor the names of his
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * CODE MODIFIED AND PACKAGE CREATED BY ANDREW FILGUEIRAS, 2020.
- */
-
 /* global define */
 (function(root, factory) {  // eslint-disable-line
     if (typeof define === 'function' && define.amd) {
@@ -44,21 +11,21 @@
     }
   }(this, function() {
     "use strict";
-  
+
     const topWindow = this;
-  
+
     /** @module webgl-utils */
-  
+
     function isInIFrame(w) {
       w = w || topWindow;
       return w !== w.top;
     }
-  
+
     if (!isInIFrame()) {
       console.log("%c%s", 'color:blue;font-weight:bold;', 'for more about webgl-utils.js see:');  // eslint-disable-line
       console.log("%c%s", 'color:blue;font-weight:bold;', 'http://webgl2fundamentals.org/webgl/lessons/webgl-boilerplate.html');  // eslint-disable-line
     }
-  
+
     /**
      * Wrapped logging function.
      * @param {string} msg The message to log.
@@ -72,16 +39,16 @@
         }
       }
     }
-  
-  
+
+
     /**
      * Error Callback
      * @callback ErrorCallback
      * @param {string} msg error message.
      * @memberOf module:webgl-utils
      */
-  
-  
+
+
     /**
      * Loads a shader.
      * @param {WebGLRenderingContext} gl The WebGLRenderingContext to use.
@@ -94,13 +61,13 @@
       const errFn = opt_errorCallback || error;
       // Create the shader object
       const shader = gl.createShader(shaderType);
-  
+
       // Load the shader source
       gl.shaderSource(shader, shaderSource);
-  
+
       // Compile the shader
       gl.compileShader(shader);
-  
+
       // Check the compile status
       const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
       if (!compiled) {
@@ -110,10 +77,10 @@
         gl.deleteShader(shader);
         return null;
       }
-  
+
       return shader;
     }
-  
+
     /**
      * Creates a program, attaches shaders, binds attrib locations, links the
      * program and calls useProgram.
@@ -140,20 +107,20 @@
         });
       }
       gl.linkProgram(program);
-  
+
       // Check the link status
       const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
       if (!linked) {
           // something went wrong with the link
           const lastError = gl.getProgramInfoLog(program);
           errFn("Error in program linking:" + lastError);
-  
+
           gl.deleteProgram(program);
           return null;
       }
       return program;
     }
-  
+
     /**
      * Loads a shader from a script tag.
      * @param {WebGLRenderingContext} gl The WebGLRenderingContext to use.
@@ -172,7 +139,7 @@
         throw ("*** Error: unknown script element" + scriptId);
       }
       shaderSource = shaderScript.text;
-  
+
       if (!opt_shaderType) {
         if (shaderScript.type === "x-shader/x-vertex") {
           shaderType = gl.VERTEX_SHADER;
@@ -182,17 +149,17 @@
           throw ("*** Error: unknown shader type");
         }
       }
-  
+
       return loadShader(
           gl, shaderSource, opt_shaderType ? opt_shaderType : shaderType,
           opt_errorCallback);
     }
-  
+
     const defaultShaderType = [
       "VERTEX_SHADER",
       "FRAGMENT_SHADER",
     ];
-  
+
     /**
      * Creates a program from 2 script tags.
      *
@@ -217,7 +184,7 @@
       }
       return createProgram(gl, shaders, opt_attribs, opt_locations, opt_errorCallback);
     }
-  
+
     /**
      * Creates a program from 2 sources.
      *
@@ -236,13 +203,14 @@
     function createProgramFromSources(
         gl, shaderSources, opt_attribs, opt_locations, opt_errorCallback) {
       const shaders = [];
+
       for (let ii = 0; ii < shaderSources.length; ++ii) {
         shaders.push(loadShader(
             gl, shaderSources[ii], gl[defaultShaderType[ii]], opt_errorCallback));
       }
       return createProgram(gl, shaders, opt_attribs, opt_locations, opt_errorCallback);
     }
-  
+
     /**
      * Resize a canvas to match the size its displayed.
      * @param {HTMLCanvasElement} canvas The canvas to resize.
@@ -262,13 +230,12 @@
       }
       return false;
     }
-  
+
     return {
       createProgram: createProgram,
       createProgramFromScripts: createProgramFromScripts,
       createProgramFromSources: createProgramFromSources,
       resizeCanvasToDisplaySize: resizeCanvasToDisplaySize,
     };
-  
+
   }));
-  
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,126 @@
-function Hello(name: string): void {
-    console.log(`Hello ${name}`);
+/*
+ * Copyright 2012, Gregg Tavares.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Gregg Tavares. nor the names of his
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ------ CODE MODIFIED AND PACKAGE CREATED BY ANDREW FILGUEIRAS, 2020. -------
+ */
+
+// const defaultShaderType = ['VERTEX_SHADER', 'FRAGMENT_SHADER'];
+
+/** Make canvas fit with no problems at the screen */
+export function resizeCanvas(canvas: HTMLCanvasElement): void {
+  // lookup the size the browser is displaying the canvas
+  const displayWidth = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+
+  // const gl = canvas.getContext('webgl2');
+
+  // const test = gl.VERTEX_SHADER;
+
+  // check if the canvas is not the same size
+  if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
+    // make the canvas the same size
+    canvas.width = displayWidth;
+    canvas.height = displayHeight;
+  }
+}
+
+/** Creates a shader based on a fragment or vertex GLSL file */
+export function createShader(
+  gl: WebGL2RenderingContext,
+  type: number,
+  source: string,
+): WebGLShader {
+  const shader = gl.createShader(type);
+
+  if (shader == null) throw new Error('This shader is not valid!');
+
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!compiled) {
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+
+    throw new Error('Error when trying to compile shader');
+  }
+  return shader;
+}
+
+/** Links shaders into a program */
+export function createProgram(
+  gl: WebGL2RenderingContext,
+  shaders: WebGLShader[],
+): WebGLProgram {
+  const program = gl.createProgram();
+
+  if (program == null)
+    throw new Error('WebGL context error, something went wrong!');
+
+  shaders.forEach(shader => {
+    gl.attachShader(program, shader);
+  });
+
+  gl.linkProgram(program);
+
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+  if (!success) {
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+
+    throw new Error('Error when trying to link the shaders into a program.');
+  }
+  return program;
+}
+
+/** Create a program based on a shader and a fragment source */
+export function createProgramFromSources(
+  gl: WebGL2RenderingContext,
+  vertexShaderSource: string,
+  fragmentShaderSource: string,
+): WebGLProgram {
+  // const shaders = [];
+
+  // shaderSources.forEach(source => {
+  //   shaders.push(createShader(gl, gl[defaultShaderType[i]], source));
+  // });
+
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource,
+  );
+
+  const sources = [vertexShader, fragmentShader];
+
+  return createProgram(gl, sources);
 }
